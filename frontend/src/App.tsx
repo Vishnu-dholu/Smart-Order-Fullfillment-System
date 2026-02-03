75e824c0cc86a4ef691bf5ce30b81f46b90546a9
Parva Parmar <parva004cp@gmail.com>
2026-02-03 12:48:19 +0530

    fe structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-grey-100 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-auth-primary">
+          Smart Order Fulfillment System
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
