d849fb22a3f077890b14a04f2f462f8de2d3e524
Vishnu Dholu <dholuvishnu10@users.noreply.github.com>
2026-02-11 18:37:00 +0530

    dimaefi

---
+++ services/spring/order_service/src/main/java/com/smartfulfillment/order_service/dto/OrderRequest.java
@@ -0,0 +1,4 @@
+package com.smartfulfillment.order_service.dto;
+
+public class OrderRequest {
+}
--- services/spring/order_service/src/main/java/com/smartfulfillment/order_service/entity/Order.java
+++ services/spring/order_service/src/main/java/com/smartfulfillment/order_service/entity/Order.java
@@ -1,7 +1,38 @@
 package com.smartfulfillment.order_service.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private UUID userId;
+
+    private UUID productId;
+
+    private int quantity;
+
+    private BigDecimal totalPrice;
+
+    private String status;
+
+    private UUID warehouseId;
+
+    @CreationTimestamp
+    private LocalDateTime orderDate;
 }
