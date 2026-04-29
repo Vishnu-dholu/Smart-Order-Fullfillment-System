d97e561e3133e90f634279059ce8d661c19e4643
Vishnu Dholu <dholuvishnu10@users.noreply.github.com>
2026-04-29 20:16:43 +0530

    initialize IEEE paper draft and finalize K6 SLAs

--- load-tests/inventory_benchmark.js
+++ load-tests/inventory_benchmark.js
@@ -57,6 +57,7 @@
 
 
 import { check, sleep } from 'k6';
+import exec from 'k6/execution';
 import http from 'k6/http';
 
 export const options = {
@@ -80,19 +81,25 @@ export const options = {
 const TARGET_URL = __ENV.TARGET_URL || 'http://localhost:8082/products';
 
 const PRODUCT_IDS = [
-    'd2a2f90f-0af3-4e56-98bb-e7279bfc8a72',
-    '524a62c0-43ee-49a3-9dda-2a80398cdeba',
-    'c193ad20-b449-4698-8ac0-3ee7365e805c',
-    'aaa7b712-03d2-42a7-81cf-d21bc911cac1',
     'e537f905-b41a-4ac1-bbb0-f0ad4f7d9c79',
+    'a733e7b3-76f2-47af-81b0-33d2c35ffb10',
+    'e2abc8a3-29d9-4f58-9c5a-ca1ffc38a6a0',
+    '7bd55cb2-0574-46de-8ce1-a950f471d9a6',
+    // 'aaa7b712-03d2-42a7-81cf-d21bc911cac1',
+    'c529aca1-c634-4627-abfa-44a2de730499',
     '27131e33-b13f-4d64-b2e1-e6e94d7ba339',
+    // '524a62c0-43ee-49a3-9dda-2a80398cdeba',
+    'c193ad20-b449-4698-8ac0-3ee7365e805c',
     '7c4cba32-ca97-4c8d-b74f-c2f433d2180a',
-    '7bd55cb2-0574-46de-8ce1-a950f471d9a6'
+    '3588af42-6f2c-4807-9167-8fa78861cac2',
+    'd2a2f90f-0af3-4e56-98bb-e7279bfc8a72'
 ];
 
 export default function () {
     // Randomly pick a product ID to view
-    const randomProductId = PRODUCT_IDS[Math.floor(Math.random() * PRODUCT_IDS.length)];
+    // const randomProductId = PRODUCT_IDS[Math.floor(Math.random() * PRODUCT_IDS.length)];
+    const randomProductId =
+        PRODUCT_IDS[(exec.scenario.iterationInTest) % PRODUCT_IDS.length];
 
     // Send a GET request
     let res = http.get(`${TARGET_URL}/${randomProductId}`);
--- load-tests/order_benchmark.js
+++ load-tests/order_benchmark.js
@@ -83,6 +83,7 @@
 
 
 import { check, sleep } from 'k6';
+import exec from 'k6/execution';
 import http from 'k6/http';
 
 export const options = {
@@ -107,19 +108,26 @@ const USER_ID = __ENV.USER_ID || '96101f4b-b0ce-4178-9a38-b2720b1a097c';
 
 // Array of products that currently have > 0 stock in the database
 const PRODUCT_IDS = [
-    'd2a2f90f-0af3-4e56-98bb-e7279bfc8a72',
-    '524a62c0-43ee-49a3-9dda-2a80398cdeba',
-    'c193ad20-b449-4698-8ac0-3ee7365e805c',
-    'aaa7b712-03d2-42a7-81cf-d21bc911cac1',
     'e537f905-b41a-4ac1-bbb0-f0ad4f7d9c79',
+    'a733e7b3-76f2-47af-81b0-33d2c35ffb10',
+    'e2abc8a3-29d9-4f58-9c5a-ca1ffc38a6a0',
+    '7bd55cb2-0574-46de-8ce1-a950f471d9a6',
+    // 'aaa7b712-03d2-42a7-81cf-d21bc911cac1',
+    'c529aca1-c634-4627-abfa-44a2de730499',
     '27131e33-b13f-4d64-b2e1-e6e94d7ba339',
+    // '524a62c0-43ee-49a3-9dda-2a80398cdeba',
+    'c193ad20-b449-4698-8ac0-3ee7365e805c',
     '7c4cba32-ca97-4c8d-b74f-c2f433d2180a',
-    '7bd55cb2-0574-46de-8ce1-a950f471d9a6'
+    '3588af42-6f2c-4807-9167-8fa78861cac2',
+    'd2a2f90f-0af3-4e56-98bb-e7279bfc8a72'
 ];
 
 export default function () {
     // Randomly pick a product ID for this specific virtual user's order
-    const randomProductId = PRODUCT_IDS[Math.floor(Math.random() * PRODUCT_IDS.length)];
+    // const randomProductId = PRODUCT_IDS[Math.floor(Math.random() * PRODUCT_IDS.length)];
+
+    const randomProductId =
+        PRODUCT_IDS[(exec.scenario.iterationInTest) % PRODUCT_IDS.length];
 
     const payload = JSON.stringify({
         shippingAddress: "123 Benchmark Ave, Load Test City",
@@ -151,7 +159,12 @@ export default function () {
 
     // IF IT FAILS, TELL US WHY AND WHICH PRODUCT FAILED
     if (res.status !== 201) {
-        console.log(`Failed! Status: ${res.status} | Product: ${randomProductId} | Body: ${res.body}`);
+        console.error(JSON.stringify({
+            status: res.status,
+            product: randomProductId,
+            body: res.body,
+            duration: res.timings.duration
+        }));
     }
 
     sleep(1);
